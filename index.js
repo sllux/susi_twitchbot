42a812d181975e17a0cac5604f04aef4eaf1380f
sllux <52381814+sllux@users.noreply.github.com>
2019-10-26 20:30:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,57 +30,6 @@ var client = new tmi.client(options);
 // Connect the client to the server
 client.connect();
 
-client.on('chat', function(channel, userstate, message, self){
-	var u = message.split('@');
-	if(u[1]){ // checking if someone is tagged
-		var name = u[1].substring(0, (process.env.USERNAME).length);
-
-		if(name === process.env.USERNAME){ // checking if SUSI is tagged
-
-			// Setting options to make a successful call to SUSI API
-			var options1 = {
-				method: 'GET',
-				url: 'http://api.susi.ai/susi/chat.json',
-				qs:
-				{
-					timezoneOffset: '-300',
-					q: u[1].substring((process.env.USERNAME).length + 1, u[1].length)
-				}
-			};
-
-			request(options1, function(error, response, body) {
-				if (error) throw new Error(error);
-
-				if((JSON.parse(body)).answers[0]) {
-					var data = JSON.parse(body);
-					if(data.answers[0].actions[0].type === "table") {
-						ans = userstate['display-name'];
-						let colNames = data.answers[0].actions[0].columns;
-						let lengthOfTable = data.answers[0].metadata.count;
-						if(lengthOfTable > 4) {
-							ans += " Due to message limit, only 4 results are shown:--- ";
-						} else {
-							ans += " Results are shown below:--- ";
-						}
-						for(let i=0; i<((lengthOfTable>4)?4:lengthOfTable); i++) {
-							for(let colNo in colNames) {
-								ans += `${colNames[colNo]} : `;
-								ans += `${data.answers[0].data[i][colNo]}, `;
-							}
-							ans += " | ";
-						}
-					} else {
-						ans = userstate['display-name'] + " " + data.answers[0].actions[0].expression;
-					}
-				} else {
-					ans = userstate['display-name'] + " Sorry, I could not understand what you just said."
-				}
-
-				client.action(userChannel, ans);
-			});
-		}
-	}
-});
 
 client.on('connected', function(address, port){
 	client.action(userChannel, `Hi, I'm SUSI. Mention me using @${process.env.USERNAME} to chat with me.`);
